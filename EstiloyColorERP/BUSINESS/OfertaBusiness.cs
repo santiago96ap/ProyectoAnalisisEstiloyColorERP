0d18b3b1a12c54d4e53ec8688062d2dcafc35f17
xCramerx <31193303+xCramerx@users.noreply.github.com>
2017-10-03 15:03:11 -0600

    Creacion de ofertas (Domain, Data, Business)

---
+++ EstiloyColorERP/BUSINESS/OfertaBusiness.cs
@@ -0,0 +1,51 @@
+﻿using DATA;
+using DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BUSINESS
+{
+    public class OfertaBusiness
+    {
+        //Atributos
+        private OfertaData ofertaData;
+
+        public OfertaBusiness()
+        {
+            this.ofertaData = new OfertaData();
+        }//constructor
+
+        public Boolean insertarOfertaCategoria(Categoria categoria, Oferta oferta)
+        {
+            return this.ofertaData.insertarOfertaCategoria(categoria, oferta);
+        }//insertarOfertaCategoria
+
+        public Boolean insertarOfertaProducto(Producto producto, Oferta oferta)
+        {
+            return this.ofertaData.insertarOfertaProducto(producto, oferta);
+        }//insertarOfertaProducto
+
+        public Boolean eliminarOferta(Oferta oferta)
+        {
+            return this.ofertaData.eliminarOferta(oferta);
+        }//eliminarOferta
+
+        public Boolean editarOferta(Oferta oferta)
+        {
+            return this.ofertaData.editaroferta(oferta);
+        }//editarOferta
+
+        public Oferta obtenerOferta()
+        {
+            return this.ofertaData.obtenerOferta();
+        }//obtenerOferta
+
+        public LinkedList<Oferta> obtenerOfertas()
+        {
+            return this.ofertaData.obtenerOferta();
+        }//obtenerOfertas
+
+    }//fin de clase OfertaBusiness
+}//fin de namespace
---
+++ EstiloyColorERP/DOMAIN/Oferta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DATA
+{
+    public class Oferta
+    {
+        //Atributos
+        private int idProducto;
+        private int idCategoria;
+        //Atributos fijos
+        private String fechaInicio;
+        private String fechaFinal;
+        private float descuento;
+
+        public Oferta()
+        {
+
+            this.idProducto = 0;
+            this.idCategoria = 0;
+
+            this.fechaInicio = "";
+            this.fechaFinal = "";
+            this.descuento = 0;
+
+        }//constructor por defecto
+
+        public Oferta(int idCategoria, String fechainicio, String fechaFinal, float descuento)
+        {
+            this.idCategoria = idCategoria;
+            this.fechaInicio = fechaInicio;
+            this.fechaFinal = fechaFinal;
+            this.descuento = descuento;
+        }//constructor sobrecargado Categoria
+
+        public Oferta(int idProducto, String fechainicio, String fechaFinal, float descuento)
+        {
+            this.idProducto = idProducto;
+            this.fechaInicio = fechaInicio;
+            this.fechaFinal = fechaFinal;
+            this.descuento = descuento;
+        }//constructor sobrecargado Producto
+
+    }//fin de la clase
+}//fin de namespace
