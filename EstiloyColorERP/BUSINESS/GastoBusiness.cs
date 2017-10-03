fdd7f8b69fe47c38b20ad2ef5cf13bee5e1852f2
xCramerx <31193303+xCramerx@users.noreply.github.com>
2017-10-03 11:08:15 -0600

    Creación de Gasto (Data, Business, Domain)

---
+++ EstiloyColorERP/BUSINESS/GastoBusiness.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BUSINESS
+{
+    public class GastoBusiness
+    {
+        //Atributos
+        GastoData gastoData;
+
+        public GastoBusiness()
+        {
+            gastoData = new GastoData();
+        }//constructor
+
+        public Boolean insertarGasto(Gasto gasto, Vendedor venedor)
+        {
+            this.gastoData.insertarGasto(gasto, venedor);
+        }//insertarGasto
+
+        public Boolean eliminarGasto(Gasto gasto)
+        {
+            this.gastoData.eliminarGasto(gasto);
+        }//eliminarGasto
+
+        public Boolean editarGasto(Gasto gasto)
+        {
+            this.gastoData.editarGasto(gasto);          
+        }//editarGasto
+
+        public Boolean obtenerGasto()
+        {
+            this.gastoData.obtenerGasto();
+        }//obtenerGasto
+
+        public Boolean obtenerGastos()
+        {
+            this.gastoData.obtenerGastos();
+        }//obtenerGastos
+
+    }//fin de la clase
+}//fin del namespace
--- EstiloyColorERP/DOMAIN/Gasto.cs
+++ EstiloyColorERP/DOMAIN/Gasto.cs
@@ -7,14 +7,101 @@ namespace DOMAIN
 {
     public class Gasto
     {
-
+        //Atributos
         private String fecha;
         private String hora;
         private String concepto;
         private float total;
         private String vendedor;
+        
+        public Gasto()
+        {
 
+            this.Fecha = "";
+            this.Hora = "";
+            this.Concepto = "";
+            this.Total = "";
+            this.Vendedor = "";
+
+        }//constructor por defecto
+
+        public Gasto(String fecha, String hora, String concepto, float total, String vendedor)
+        {
+
+            this.Fecha = fecha;
+            this.Hora = hora;
+            this.Concepto = concepto;
+            this.Total = total;
+            this.Vendedor = vendedor;
+
+        }//constructor sobrecargado
+
+        //OBTENER e INSERTAR
+        public string Fecha
+        {
+            get
+            {
+                return fecha;
+            }
+
+            set
+            {
+                fecha = value;
+            }
+        }
+
+        public string Hora
+        {
+            get
+            {
+                return hora;
+            }
+
+            set
+            {
+                hora = value;
+            }
+        }
+
+        public string Concepto
+        {
+            get
+            {
+                return concepto;
+            }
+
+            set
+            {
+                concepto = value;
+            }
+        }
+
+        public float Total
+        {
+            get
+            {
+                return total;
+            }
+
+            set
+            {
+                total = value;
+            }
+        }
+
+        public string Vendedor
+        {
+            get
+            {
+                return vendedor;
+            }
+
+            set
+            {
+                vendedor = value;
+            }
+        }
 
     }//fin de clase
 
-}//namespace
+}//fin de namespace
