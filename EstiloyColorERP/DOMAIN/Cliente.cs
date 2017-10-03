2be9fa6b11548e1e3b90c9b421617d8f3dab0cbe
JEstiben <31193316+JEstiben@users.noreply.github.com>
2017-10-03 10:25:42 -0600

    Objeto Cliente y Apartado

--- EstiloyColorERP/DOMAIN/Cliente.cs
+++ EstiloyColorERP/DOMAIN/Cliente.cs
@@ -12,8 +12,86 @@ namespace DOMAIN
         private String nombre;
         private String apellidos;
         private String telefono;
-        private String dirrecion;
+        private String direccion;
         private String correo;
 
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }//get id
+
+            set
+            {
+                id = value;
+            }//set id
+        }//Id
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }//get nombre
+
+            set
+            {
+                nombre = value;
+            }//set nombre
+        }//Nombre
+
+        public string Apellidos
+        {
+            get
+            {
+                return apellidos;
+            }//get apellidos
+
+            set
+            {
+                apellidos = value;
+            }//set apellidos
+        }//Apellidos
+
+        public string Telefono
+        {
+            get
+            {
+                return telefono;
+            }//get telefono
+
+            set
+            {
+                telefono = value;
+            }//set telefono
+        }//Telefono
+
+        public string Direccion
+        {
+            get
+            {
+                return direccion;
+            }//get direccion
+
+            set
+            {
+                direccion = value;
+            }//set direccion
+        }//Direccion
+
+        public string Correo
+        {
+            get
+            {
+                return correo;
+            }//get correo
+
+            set
+            {
+                correo = value;
+            }//set correo
+        }//Correo
     }//class
 }//namespace
