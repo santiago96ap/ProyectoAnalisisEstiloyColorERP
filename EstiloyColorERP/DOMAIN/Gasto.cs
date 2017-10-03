24fc58f31abc840507992cdfb8830b7842b26ffc
xCramerx <31193303+xCramerx@users.noreply.github.com>
2017-10-03 10:23:44 -0600

    Creación de Gasto

---
+++ EstiloyColorERP/DOMAIN/Gasto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOMAIN
+{
+    public class Gasto
+    {
+
+        private String fecha;
+        private String hora;
+        private String concepto;
+        private float total;
+        private String vendedor;
+
+
+    }//fin de clase
+
+}//namespace
