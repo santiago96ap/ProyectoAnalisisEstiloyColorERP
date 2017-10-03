de3b0bd92fe746a1baf779736b34acebdaab57a5
yuli2596 <yuli.brenes9231@gmail.com>
2017-10-03 09:38:33 -0600

    comentarios set y get de agenda y ventas

--- EstiloyColorERP/DOMAIN/Agenda.cs
+++ EstiloyColorERP/DOMAIN/Agenda.cs
@@ -34,65 +34,65 @@ namespace DOMAIN
             get
             {
                 return fecha;
-            }
+            }//get fecha
 
             set
             {
                 fecha = value;
-            }
-        }
+            }//set fecha
+        }//set y get fecha
 
         public string Hora
         {
             get
             {
                 return hora;
-            }
+            }//get hora
 
             set
             {
                 hora = value;
-            }
-        }
+            }//set hora
+        }//set y get hora
 
         public string Actividad
         {
             get
             {
                 return actividad;
-            }
+            }//get actividad
 
             set
             {
                 actividad = value;
-            }
-        }
+            }//set actividad
+        }//set y get actividad
 
         public string Direccion
         {
             get
             {
                 return direccion;
-            }
+            }//get direccion
 
             set
             {
                 direccion = value;
-            }
-        }
+            }//set direccion
+        }//set y get direccion
 
         public int Cliente
         {
             get
             {
                 return cliente;
-            }
+            }//get cliente
 
             set
             {
                 cliente = value;
-            }
-        }
+            }//set cliente
+        }//set y get cliente
 
        
     }//class
--- EstiloyColorERP/DOMAIN/Venta.cs
+++ EstiloyColorERP/DOMAIN/Venta.cs
@@ -44,120 +44,116 @@ namespace DOMAIN
             get
             {
                 return fecha;
-            }
+            }//get fecha
 
             set
             {
                 fecha = value;
-            }
-        }
+            }//set fecha
+        }//set y get fecha
 
         public string Hora
         {
             get
             {
                 return hora;
-            }
+            }//get hora
 
             set
             {
                 hora = value;
-            }
-        }
+            }//set hora
+        }//set y get hora
 
         public int Cliente
         {
             get
             {
                 return cliente;
-            }
+            }//get cliente
 
             set
             {
                 cliente = value;
-            }
-        }
+            }//set cliente
+        }//set y get cliente
 
         public int Vendedor
         {
             get
             {
                 return vendedor;
-            }
+            }//get vendedor
 
             set
             {
                 vendedor = value;
-            }
-        }
+            }//set vendedor
+        }//set y get vendedor
 
         public string TipoServicio
         {
             get
             {
                 return tipoServicio;
-            }
+            }//get tipoServicio
 
             set
             {
                 tipoServicio = value;
-            }
-        }
+            }//set tipoServicio
+        }//set y get tipoServicio
 
         public int ArticuloComprado
         {
             get
             {
                 return articuloComprado;
-            }
+            }//get articulo comprado
 
             set
             {
                 articuloComprado = value;
-            }
-        }
+            }//set articulo comprado
+        }//set y get articuloComprado
 
         public float SubTotal
         {
             get
             {
                 return subTotal;
-            }
+            }//get subtotal
 
             set
             {
                 subTotal = value;
-            }
-        }
+            }//set subtotal
+        }//set y get sub total
 
         public float Total
         {
             get
             {
                 return total;
-            }
+            }//get total
 
             set
             {
                 total = value;
-            }
-        }
+            }//set total
+        }//set y get total
 
         public string TipoPago
         {
             get
             {
                 return tipoPago;
-            }
+            }//get tipo de pago
 
             set
             {
                 tipoPago = value;
-            }
-        }
-
-
-
-
+            }//set tipo de pago
+        }//set y get tipo de pago
     }//class
 }//namespace
