1ad1b2a3f9aeba4f5c350db076acf1d23f8e5dbb
santiago96ap <santiagoastoper@gmail.com>
2017-10-03 14:10:13 -0600

    Creación de IngresoData

---
+++ EstiloyColorERP/DATA/IngresoData.cs
@@ -0,0 +1,42 @@
+﻿using DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DATA
+{
+    public class IngresoData
+    {
+        private String stringConeccion;
+
+        public IngresoData(string stringConeccion)
+        {
+            this.stringConeccion = stringConeccion;
+        }//constructor sobreCargado
+
+
+        public Boolean insertarIngreso(Ingreso ingreso)
+        {
+            return false;
+        }//insertarIngreso
+
+        public Boolean actualizarIngreso(Ingreso ingreso)
+        {
+            return false;
+        }//actualizarIngreso
+
+        public Boolean eliminarIngreso(Ingreso ingreso)
+        {
+            return false;
+        }//eliminarIngreso
+
+        public LinkedList<Ingreso> obtenerIngresos(String fechaInicial, String fechaFinal)
+        {
+            return null;
+        }//obtnerIngresos
+
+
+
+    }//class
+}//namespace
