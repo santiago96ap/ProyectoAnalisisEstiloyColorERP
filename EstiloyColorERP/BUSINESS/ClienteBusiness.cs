7ffca13fc4b11dda341b22ab6769b9b65c971245
JEstiben <31193316+JEstiben@users.noreply.github.com>
2017-10-03 12:00:58 -0600

    Creacion de ClienteBusiness y ApartadoBusiness

---
+++ EstiloyColorERP/BUSINESS/ClienteBusiness.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DOMAIN;
+using DATA;
+
+namespace BUSINESS
+{
+    public class ClienteBusiness
+    {
+        private ClienteData clienteData;
+
+        public ClienteBusiness()
+        {
+            this.clienteData = new ClienteData();
+        }//constructor
+
+        public Boolean insertarCliente(Cliente cliente)
+        {
+            return this.clienteData.insertarCliente(cliente);
+        }//insertarCliente
+
+        public Boolean actualizarCliente(Cliente cliente)
+        {
+            return this.clienteData.actualizarCliente(cliente);
+        }//actualizarCliente
+
+        public LinkedList<Cliente> obtenerClientes()
+        {
+            return this.clienteData.obtenerClientes();
+        }//obtenerClientes
+
+        public Venta obtenerCliente(Cliente cliente)
+        {
+            return this.clienteData.obtenerCliente(cliente);
+        }//obtenerCliente
+
+    }//class
+}//namespace
