1f34860fbc68bdef8df401fc11fe95fbfb115747
JEstiben <31193316+JEstiben@users.noreply.github.com>
2017-10-03 09:41:23 -0600

    Master Page

---
+++ EstiloyColorERP/DOMAIN/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOMAIN
+{
+    public class Cliente
+    {
+
+        private String id;
+        private String nombre;
+        private String apellidos;
+        private String telefono;
+        private String dirrecion;
+        private String correo;
+
+    }//class
+}//namespace
