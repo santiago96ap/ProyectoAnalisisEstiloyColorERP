2c50d12bee6fa4cb884e23b825adad838b7d80c1
JEstiben <31193316+JEstiben@users.noreply.github.com>
2017-11-03 12:10:31 -0600

    Cambios

--- EstiloyColorERP/EstiloyColorERP/Site.Master.designer.cs
+++ EstiloyColorERP/EstiloyColorERP/Site.Master.designer.cs
@@ -13,6 +13,42 @@ namespace EstiloyColorERP {
     public partial class Site {
         
         /// <summary>
+        /// lbCorreo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbCorreo;
+        
+        /// <summary>
+        /// tbNombre control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbNombre;
+        
+        /// <summary>
+        /// lbContraseña control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbContraseña;
+        
+        /// <summary>
+        /// tbContraseña control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbContraseña;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
