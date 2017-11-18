378f4182bad8df941b0589696b720d7dd4ee627d
JEstiben <31193316+JEstiben@users.noreply.github.com>
2017-11-17 21:51:17 -0600

    Actualizar apartado

--- EstiloyColorERP/EstiloyColorERP/publico/ActualizarApartadoVista.aspx.cs
+++ EstiloyColorERP/EstiloyColorERP/publico/ActualizarApartadoVista.aspx.cs
@@ -14,6 +14,8 @@ namespace EstiloyColorERP
     {
         private ApartadoBusiness apartadoBusiness;
         private LinkedList<Apartado> apartados;
+        private ClienteBusiness clienteBusiness = new ClienteBusiness();
+        private LinkedList<Cliente> clientes;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -25,42 +27,141 @@ namespace EstiloyColorERP
 
         protected void cargarInformacion()
         {
-            this.apartadoBusiness = new ApartadoBusiness();
-            this.apartados = this.apartadoBusiness.obtenerApartados();
-            DataTable table = new DataTable("Apartados");
-
-            table.Columns.Add(new DataColumn("id", typeof(int)));
-            table.Columns.Add(new DataColumn("teléfono Cliente", typeof(string)));
-            table.Columns.Add(new DataColumn("Saldo final", typeof(string)));
-            table.Columns.Add(new DataColumn("Fecha de inicio", typeof(string)));
-            table.Columns.Add(new DataColumn("Vencimiento", typeof(string)));
-            table.Columns.Add(new DataColumn("Estado", typeof(string)));
-            table.Columns.Add(new DataColumn("Número de factura asociado", typeof(int)));
-
-
-            foreach (Apartado aActual in this.apartados)
-
+            this.clientes = this.clienteBusiness.obtenerClientes();
+            bool flag = false;
+            if (String.IsNullOrWhiteSpace(this.tbCliente.Text))
+            {//si existe un tb en blanco se indica la agenda y no se aplica ningún cambio
+                ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('Error, campos en blanco')", true);
+            }
+            else
             {
-                DataRow row = table.NewRow();
-
-                if (aActual.IdCliente.Equals(tbCliente.Text))
+                foreach (Cliente clienteActual in this.clientes)
                 {
-                    row["id"] = aActual.Id;
-                    row["teléfono Cliente"] = aActual.IdCliente;
-                    row["Saldo final"] = aActual.Monto;
-                    row["Fecha de inicio"] = aActual.FechaInicio;
-                    row["Vencimiento"] = aActual.FechaFinal;
-                    row["Estado"] = aActual.Estado;
-                    row["Número de factura asociado"] = aActual.NumFactura;
-                }//if
-                    
+                    if (clienteActual.Telefono.Equals(this.tbCliente.Text))
+                    {
+                        flag = true;
+                    }//if para validar que el cliente ingresado exista
+                }//For para recorrer los clientes que estan en el sistema
+                if (flag)
+                {
+                    this.apartadoBusiness = new ApartadoBusiness();
+                    this.apartados = this.apartadoBusiness.obtenerApartados();
+                    DataTable table = new DataTable("Apartados");
 
-                table.Rows.Add(row);
-            }//foreach
+                    table.Columns.Add(new DataColumn("id", typeof(int)));
+                    table.Columns.Add(new DataColumn("teléfono Cliente", typeof(string)));
+                    table.Columns.Add(new DataColumn("Saldo final", typeof(string)));
+                    table.Columns.Add(new DataColumn("Fecha de inicio", typeof(string)));
+                    table.Columns.Add(new DataColumn("Vencimiento", typeof(string)));
+                    table.Columns.Add(new DataColumn("Estado", typeof(string)));
+                    table.Columns.Add(new DataColumn("Número de factura asociado", typeof(int)));
 
-            this.gvCategorias.DataSource = table;
-            this.gvCategorias.DataBind();
+
+                    foreach (Apartado aActual in this.apartados)
+
+                    {
+                        DataRow row = table.NewRow();
+
+                        if (aActual.IdCliente.Equals(tbCliente.Text))
+                        {
+                            row["id"] = aActual.Id;
+                            row["teléfono Cliente"] = aActual.IdCliente;
+                            row["Saldo final"] = aActual.Monto;
+                            row["Fecha de inicio"] = aActual.FechaInicio;
+                            row["Vencimiento"] = aActual.FechaFinal;
+                            row["Estado"] = aActual.Estado;
+                            row["Número de factura asociado"] = aActual.NumFactura;
+                        }//if
+
+
+                        table.Rows.Add(row);
+                    }//foreach
+
+                    this.gvCategorias.DataSource = table;
+                    this.gvCategorias.DataBind();
+                    this.tbID.Text = "";
+                    this.tbSaldo.Text = "";
+                    this.tbAbono.Text = "";
+                    this.tbID.Enabled = false;
+                    this.tbSaldo.Enabled = false;
+                    this.tbAbono.Enabled = false; 
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('El cliente ingresado no existe')", true);
+                }//if-else para identificar si se encontro el cliente
+            }//else - no hay datos en blanco
         }//cargarInformacion
+
+        /// <summary>
+        /// Este métdodo es para cargar la información necesaria del apartado de un cliente 
+        /// </summary>
+
+        protected void cargarAbono()
+        {
+            this.clientes = this.clienteBusiness.obtenerClientes();
+            bool flag = false;
+            if (String.IsNullOrWhiteSpace(this.tbCliente.Text))
+            {//si existe un tb en blanco se indica la agenda y no se aplica ningún cambio
+                ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('Error, campos en blanco')", true);
+            }
+            else
+            {
+                foreach (Cliente clienteActual in this.clientes)
+                {
+                    if (clienteActual.Telefono.Equals(this.tbCliente.Text))
+                    {
+                        flag = true;
+                    }//if para validar que el cliente ingresado exista
+                }//For para recorrer los clientes que estan en el sistema
+                if (flag)
+                {
+                    this.apartadoBusiness = new ApartadoBusiness();
+                    this.apartados = this.apartadoBusiness.obtenerApartados();
+                    DataTable table = new DataTable("Apartados");
+
+                    table.Columns.Add(new DataColumn("id", typeof(int)));
+                    table.Columns.Add(new DataColumn("teléfono Cliente", typeof(string)));
+                    table.Columns.Add(new DataColumn("Saldo final", typeof(string)));
+                    table.Columns.Add(new DataColumn("Fecha de inicio", typeof(string)));
+                    table.Columns.Add(new DataColumn("Vencimiento", typeof(string)));
+                    table.Columns.Add(new DataColumn("Estado", typeof(string)));
+                    table.Columns.Add(new DataColumn("Número de factura asociado", typeof(int)));
+
+
+                    foreach (Apartado aActual in this.apartados)
+
+                    {
+                        DataRow row = table.NewRow();
+
+                        if (aActual.IdCliente.Equals(tbCliente.Text))
+                        {
+                            row["id"] = aActual.Id;
+                            row["teléfono Cliente"] = aActual.IdCliente;
+                            row["Saldo final"] = aActual.Monto;
+                            row["Fecha de inicio"] = aActual.FechaInicio;
+                            row["Vencimiento"] = aActual.FechaFinal;
+                            row["Estado"] = aActual.Estado;
+                            row["Número de factura asociado"] = aActual.NumFactura;
+                        }//if
+
+
+                        table.Rows.Add(row);
+                    }//foreach
+
+                    this.gvCategorias.DataSource = table;
+                    this.gvCategorias.DataBind();
+                    this.tbID.Enabled = false;
+                    this.tbSaldo.Enabled = false;
+                    this.tbAbono.Enabled = false;
+                    this.btnActualizar.Enabled = false;
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('El cliente ingresado no existe')", true);
+                }//if-else para identificar si se encontro el cliente
+            }//else - no hay datos en blanco
+        }//cargarAbono
 
         /// <summary>
         /// Este método realizará la acción de seleccionar la fila del GridView 
@@ -82,6 +183,8 @@ namespace EstiloyColorERP
                 {
                     this.tbSaldo.Text = cActual.Monto.ToString();
                     this.tbID.Text = cActual.Id.ToString();
+                    this.tbAbono.Enabled = true;
+                    this.btnActualizar.Enabled = true;
                 }//If
             }//foreach
 
@@ -99,33 +202,39 @@ namespace EstiloyColorERP
         /// <param name="e"></param>
         protected void btnActualizar_Click1(object sender, EventArgs e)
         {
-            if (String.IsNullOrWhiteSpace(this.tbCliente.Text))
+            if (String.IsNullOrWhiteSpace(this.tbCliente.Text) || String.IsNullOrWhiteSpace(this.tbAbono.Text))
             {//si existe un tb en blanco se indica al usuario y no se aplica ningún cambio
-                ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('Error en los datos ingresados')", true);
+                ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('Error, campos en blanco')", true);
             }
             else
             {
-                this.apartadoBusiness = new ApartadoBusiness();
-
-                Apartado apartadoNuevo = new Apartado(int.Parse(tbID.Text), float.Parse(tbAbono.Text));
-
-                bool respuesta = this.apartadoBusiness.actualizarApartado(apartadoNuevo);
-
-                if (respuesta)// Si se actualiza la categoria se recargan los datos y se dejan los tb en blanco
+                if(int.Parse(this.tbAbono.Text)<= int.Parse(this.tbSaldo.Text))
                 {
-                    ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.success('Se actualizó exitosamente')", true);
-                    //dejar los campos de texto en blanco
+                    this.apartadoBusiness = new ApartadoBusiness();
 
-                    this.tbAbono.Text = " ";
-                    this.tbSaldo.Text = " ";
-                    this.tbID.Text = " ";
+                    Apartado apartadoNuevo = new Apartado(int.Parse(tbID.Text), float.Parse(tbAbono.Text));
 
-                    cargarInformacion();//recargar el gridview
-                }//if
+                    bool respuesta = this.apartadoBusiness.actualizarApartado(apartadoNuevo);
+
+                    if (respuesta)// Si se actualiza la categoria se recargan los datos y se dejan los tb en blanco
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.success('Se actualizó exitosamente')", true);
+                        //dejar los campos de texto en blanco
+
+                        int saldo = int.Parse(this.tbSaldo.Text) - int.Parse(this.tbAbono.Text);
+                        this.tbSaldo.Text = saldo.ToString();
+
+                        cargarAbono();//recargar el gridview
+                    }//if
+                    else
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('Se ha producido un error al procesar la solicitud')", true);
+                    }//else
+                }
                 else
                 {
-                    ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('Se ha producido un error al procesar la solicitud')", true);
-                }//else
+                    ClientScript.RegisterStartupScript(this.GetType(), "alertify", "alertify.error('El abono supera la cantidad del saldo')", true);
+                }//if-else de abono
 
             }//if
 
